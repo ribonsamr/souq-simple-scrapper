4ded09ad6f7b0583a4fb3d8cbfc68d57a8b2768c
Amr Essam <amressam01@hotmail.com>
2020-02-07 17:59:29 +0200

    Added comments

--- souq.py
+++ souq.py
@@ -1,11 +1,13 @@
-import re
 import csv
+import re
 
 import lxml
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
+
+excel_filename = 'data.xlsx'
 
 webpages = [{
     'link': 'https://deals.souq.com/eg-en/mobiles/cc/482',
@@ -54,38 +56,53 @@ webpages = [{
 def crawl(link):
     page = requests.get(link)
     page_soup = BeautifulSoup(page.text, 'lxml')
+
+    # Get items titles
     page_items = [
         i.getText().strip() for i in page_soup.select("span.itemTitle a")
     ]
+
+    # Get items prices
+    # Use regex to extract numbers only
     page_prices = [
         re.search("(^[\d,.]*)", i.getText()).group(0)
         for i in page_soup.select("h5.price span.is")
     ]
+
+    # Zip items and prices in a list and return it
     page_pairs = list(zip(page_items, page_prices))
     return page_pairs
 
 
 def write_sheet(wb, sheet_name, header, pairs, active=False):
+    """ Write an Excel Sheet """
     ws = None
+
     if active:
+        # If it's the first sheet to write
         ws = wb.active
         ws.title = sheet_name
     else:
         ws = wb.create_sheet(sheet_name)
+
+    # Write headers
     ws.cell(1, 1, header[0])
     ws.cell(1, 2, header[1])
 
+    # Write data
     for row in range(2, len(pairs) + 1):
         for col in range(1, 3):
             _ = ws.cell(column=col, row=row, value=f"{pairs[row-2][col-1]}")
 
 
+# Create new Excel file
 wb = Workbook()
 
+# Loop through webpages, get each one's data, and write it to the excel sheet
 for page in webpages:
-    print("Getting " + page['sheet_name'])
-
+    print("Getting {}... ".format(page['sheet_name']), end='', flush=True)
     content = crawl(page['link'])
+    print("Done.")
 
     write_sheet(
         wb,
@@ -94,4 +111,7 @@ for page in webpages:
         content,
         active=page.get('active') or False)
 
-wb.save(filename="data.xlsx")
+# Save the final excel file
+wb.save(filename=excel_filename)
+
+print("Data saved to '{}'".format(excel_filename))
